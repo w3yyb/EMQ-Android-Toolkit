c2801502332a31c9cddb5b8f4d376a2ecae830f1
Zhiw <karlkb81@gmail.com>
2017-04-01 10:34:12 +0800

    Support select from the list of topics when you publish

--- app/src/main/java/io/emqtt/emqandroidtoolkit/ui/activity/PublicationActivity.java
+++ app/src/main/java/io/emqtt/emqandroidtoolkit/ui/activity/PublicationActivity.java
@@ -3,19 +3,29 @@ package io.emqtt.emqandroidtoolkit.ui.activity;
 import org.eclipse.paho.client.mqttv3.MqttTopic;
 
 import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Switch;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import io.emqtt.emqandroidtoolkit.Constant;
 import io.emqtt.emqandroidtoolkit.R;
 import io.emqtt.emqandroidtoolkit.model.Publication;
+import io.emqtt.emqandroidtoolkit.model.Subscription;
 import io.emqtt.emqandroidtoolkit.ui.base.ToolBarActivity;
 import io.emqtt.emqandroidtoolkit.ui.widget.QoSChooseLayout;
+import io.emqtt.emqandroidtoolkit.util.RealmHelper;
 import io.emqtt.emqandroidtoolkit.util.TipUtil;
+import io.realm.RealmResults;
 
 public class PublicationActivity extends ToolBarActivity {
 
@@ -25,6 +35,7 @@ public class PublicationActivity extends ToolBarActivity {
     @BindView(R.id.retained) Switch mRetainedSwitch;
     @BindView(R.id.btn_publish) Button mPublishBtn;
     @BindView(R.id.linear_layout) LinearLayout mLinearLayout;
+    @BindView(R.id.spinner) Spinner mSpinner;
 
     @Override
     protected int getLayoutResId() {
@@ -33,6 +44,28 @@ public class PublicationActivity extends ToolBarActivity {
 
     @Override
     protected void setUpView() {
+
+        RealmResults<Subscription> results = RealmHelper.getInstance().queryAll(Subscription.class);
+        final List<String> list = new ArrayList<>();
+        for (Subscription result : results) {
+            list.add(result.getTopic());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, list);
+        mSpinner.setAdapter(adapter);
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String topic = list.get(position);
+                mTopic.setText(topic);
+                mTopic.setSelection(topic.length());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
     @Override
